3303e96af231131da5d741833c3957ce1fa22508
Talent Antonio <antonio.talenty@gmail.com>
2019-03-26 22:11:21 +0000

    FindById working

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -17,6 +17,23 @@ postsRouter.get('/', (req, res) => {
         .json({ error: 'The posts information could not be retrieved.' })
     })
 })
+postsRouter.get('/:id', (req, res) => {
+    db.findById(req.params.id)
+      .then(post => {
+        if (post) {
+          res.status(200).json(post)
+        } else {
+          res.status(404).json({
+               message: 'The post with the specified ID does not exist.' 
+            })
+        }
+      })
+      .catch(() =>
+        res.status(500).json({
+             error: 'The post information could not be retrieved.' 
+             })
+      )
+   })
 
 
 
